1741949b5834484121bce377b37bf386f8c707cb
Riley Denoyer <10578060+Radbuglet@users.noreply.github.com>
2020-08-26 15:27:25 -0700

    Add support for providing abstract classes (constructor syntax only works for constructible classes)

--- lib.d.ts
+++ lib.d.ts
@@ -1,24 +1,28 @@
+// Helpers
+type Class<T> = Function & { prototype: T };
+type ClassInstance<T extends Class<any>> = T extends Class<infer I> ? I : never;
+
 /**
  * Represents a class constructor/type that contains a type static field for use
  * in the provision interface.
  */
-type IProvider = (new(...args: any[]) => object) & { readonly type: symbol; };
+type IProvider = Class<any> & { readonly type: symbol; };
 
 /**
  * Generates an interface for a class which provides a class of type "InstanceType<TClass>"". If "TReal" is provided,
  * this type will be used instead of the InstanceType<TClass>. "TReal" is used when the provided class has some generic type information
  * that cannot be inferred by "InstanceType".
  */
-export type IProvides<TClass extends IProvider, TReal extends InstanceType<TClass> = never> = {
-    [_ in TClass["type"]]: TReal extends never ? InstanceType<TClass> : TReal
+export type IProvides<TClass extends IProvider, TReal extends ClassInstance<TClass> = never> = {
+    [_ in TClass["type"]]: TReal extends never ? ClassInstance<TClass> : TReal
 };
 
 /**
  * @alias IProvides
  */
-export type P$<TClass extends IProvider, TReal extends InstanceType<TClass> = never> = IProvides<TClass, TReal>;
+export type P$<TClass extends IProvider, TReal extends ClassInstance<TClass> = never> = IProvides<TClass, TReal>;
 
 /**
  * @alias IProvides
  */
-export type $<TClass extends IProvider, TReal extends InstanceType<TClass> = never> = IProvides<TClass, TReal>;
+export type $<TClass extends IProvider, TReal extends ClassInstance<TClass> = never> = IProvides<TClass, TReal>;
